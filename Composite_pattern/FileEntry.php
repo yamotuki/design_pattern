97da04f0e144f61a61ab4e7330ecf3c4c53cfc0e
Tomoya Suzuki <t-suzuki@macloud.jp>
2018-10-19 11:12:41 +0900

    composite pattern practice

--- Composite_pattern/FileEntry.php
+++ Composite_pattern/FileEntry.php
@@ -28,4 +28,11 @@ class FileEntry extends Entry
         // MEMO ここで単に $this と書いているのでは 文字列連結の時に $this->__toString() が呼ばれるから
         echo($prefix . "/" . $this . "\n");
     }
+
+    // ファイルのフルパスを得るためにはどこに変更を入れればいいか？というお題で、最初以下のメソッドを生やすことを考えたが、FileインスタンスはどこのDierctoryに含まれているか知ることができないのでここに生やしてもだめ。
+/*    public function printFullPath()
+    {
+    }*/
+    // むしろ、Rootに find() メソッドを生やして、最後全部足した後に file名で検索して表示させるみたいなことをしないとダメかも？　
+    // そこまで考えて回答を見てみると、parent情報をDirectory（とEntry）に持たせる案が書かれていた。確かにLinuxは ../ にその情報を持っているか。多分それにより pwd とかを表現している。
 }
